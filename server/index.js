f6cbb2c2329fe692439359955080fd1f8a4400ea
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-12-20 20:51:33 +0100

    Removing start color

--- server/index.js
+++ server/index.js
@@ -19,26 +19,24 @@ const make_request_url = page_number => {
 
 const filter_response = body => {
   let result = {};
-  const start_color = "#0F2027";
-  const end_colors = ["#33CB83", "#353BCE", "#CC2C2C", "#47788D", "#2CAACD", "#D0AD31", "#D033D0", "#D16D34"]
+  const colors = ["#33CB83", "#353BCE", "#CC2C2C", "#47788D", "#2CAACD", "#D0AD31", "#D033D0", "#D16D34"]
   if (body.code == 200){
     result.copyright = body.attributionText;
     const characters_results = new Array(CHARACTERS_PER_PAGE);
     // filtering each character
     body.data.results.forEach((c, index) => {
-        const n = end_colors.length;
+        const n = colors.length;
         const rnd_index = Math.floor(Math.random() * n);
         let current_character = {
           id: c.id,
           name: c.name,
           thumbnail: `${c.thumbnail.path}/portrait_incredible.jpg`,
           description: c.description,
-          start_color: start_color,
-          end_color: end_colors[rnd_index]
+          color: colors[rnd_index]
         };
         characters_results[index] = current_character;
         // removing used color
-        end_colors.splice(rnd_index, 1);
+        colors.splice(rnd_index, 1);
     });
     result.data = characters_results;
   }
