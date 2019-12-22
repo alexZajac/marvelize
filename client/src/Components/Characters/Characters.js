9f7fffdc1aadb0a64a7f4518e7d31f3b81d13904
Alexandre Zajac <zajac.alexandre@gmail.com>
2019-12-22 19:13:57 +0100

    Adding responsiveness

--- client/src/Components/Characters/Characters.js
+++ client/src/Components/Characters/Characters.js
@@ -8,29 +8,11 @@ const Characters = props => {
     // we render the row only when data has arrived, for animation to run properly
     const dataArrived = charactersData.length > 0;
 
-    const selectFirstRow = (i, c, cData) => (
-        i < cData.length/2 ? 
-            <SingleCharacter key={c.id} character={c} />
-            :
-            null
-    );
-    const selectSecondRow = (i, c, cData) => (
-        i >= cData.length/2 ? 
-            <SingleCharacter key={c.id} character={c} />
-            :
-            null
-    )
-
     return (
-        <div className={`characters ${dataArrived ? "" : "disappear"}`}>
+        <div className="characters">
             {dataArrived ?
-                <>
-                    <div className="row-characters first-part-c appear">
-                        {charactersData.map((c, i) => selectFirstRow(i, c, charactersData))}  
-                    </div>
-                    <div className="row-characters appear">
-                        {charactersData.map((c, i) => selectSecondRow(i, c, charactersData))}   
-                    </div>
+                <>   
+                    {charactersData.map((c, i) =>  <SingleCharacter key={c.id} character={c} />)}   
                 </>
                 :
                 null
